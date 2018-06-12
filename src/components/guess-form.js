d3be67040b23b596a6369e71e6e5250daf8ba0cf
KatiLong <katileighlong@gmail.com>
2018-06-11 23:27:55 -0700

    Guess Form code complete

---
+++ src/components/guess-form.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './guess-form.css';
+
+export default function GuessForm extends React.Component {
+    onSubmit(event) {
+        event.preventDefault();
+
+        if (this.props.onMakeGuess) {
+            const value = this.input.value;
+            this.props.onMakeGuess(value);
+        }
+        this.input.value = '';
+        this.input.focus();
+    }
+
+    render(){
+        return (
+            <form>
+                <input
+                    type="number"
+                    name="userGuess"
+                    id="userGuess"
+                    className="text"
+                    min="1"
+                    max="100"
+                    autoComplete="off"
+                    aria-labelledby="feedback"
+                    ref={input => (this.input = input)}
+                    required
+                />
+                <button
+                    type="submit"
+                    name="submit"
+                    id="guessButton"
+                    className="button"
+                >Guess</button>
+            </form>
+        );
+    }
+}
